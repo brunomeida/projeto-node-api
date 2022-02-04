7bbb38c87d838f66fa14881805921fa872119c7e
Bruno Medeiros <bruno.menescal@contabilizei.com.br>
2022-02-04 00:45:19 -0300

    [FEATURE][SECURITY] Add Helmet cookie-session

--- server/server.js
+++ server/server.js
@@ -1,6 +1,31 @@
+var cookieSession = require('cookie-session');
 const express = require('express');
+const helmet = require("helmet");
+
 const app = express();
+const port = 3000;
+
+app.use(helmet());
+
+app.use(cookieSession({
+    name: 'session',
+    keys: ['key1', 'key2']
+}));
+
+app.set('trust proxy', 1) // trust first proxy
+
+
+app.get('/', function (req, res, next) {
+    // Update views
+    req.session.views = (req.session.views || 0) + 1
+  
+    // Write response
+    res.end(req.session.views + ' views')
+})
+
 
 app.use('/', require('./route/postsRoute'));
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`click me: http://localhost:${port}`)
+})
